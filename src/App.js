343997b6337d54022bd7ad27373632d08881fc26
Bernardo Sciarretta <basciarretta@hotmail.com>
2022-07-02 20:51:42 -0300

    agrego carpeta components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import NavBar from './NavBar';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -15,7 +15,7 @@ const NavBar = () => {
                 <span className="navbar-toggler-icon"></span>
               </button>
               <div className="collapse navbar-collapse" id="navbarNav">
-                <ul className="navbar-nav">
+                <ul className="navbar-nav ">
                   <li className="nav-item">
                     <a className="nav-link" href="">
                       JUEGOS
