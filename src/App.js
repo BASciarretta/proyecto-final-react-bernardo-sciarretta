4c353260640ef39d8c856eac3499594f0c328bc9
Bernardo Sciarretta <basciarretta@hotmail.com>
2022-07-02 20:31:24 -0300

    acomodo elementos

--- src/App.js
+++ src/App.js
@@ -1,59 +1,11 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-
-const NavBar = () => {
-  return(
-<>
-<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-          <div className="container-fluid">
-            <a className="navbar-brand" href="">
-              BS-GAMES
-            </a>
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarNav"
-              aria-controls="navbarNav"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
-              <ul className="navbar-nav">
-                <li className="nav-item">
-                  <a className="nav-link" href="">
-                    JUEGOS
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="">
-                    CONSOLAS
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="">
-                    PC
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="">
-                    COMPONENTES
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
-</>
-  );
-}
+import NavBar from './NavBar';
 
 function App() {
   return (
     <div className="App">
-{NavBar()}
+<NavBar/>
     </div>
   );
 }
---
+++ src/NavBar.js
@@ -0,0 +1,47 @@
+const NavBar = () => {
+    return(
+  <>
+  <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="container-fluid">
+              <a className="navbar-brand" href="">
+                BS-GAMES
+              </a>
+              <button
+                className="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarNav"
+              >
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav">
+                  <li className="nav-item">
+                    <a className="nav-link" href="">
+                      JUEGOS
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="">
+                      CONSOLAS
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="">
+                      PC
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="">
+                      COMPONENTES
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+  </>
+    );
+  }
+
+  export default NavBar;
